30362050c5f4dd1ad27fee421d399f6a07bed646
callum <callumgibbons@icloud.com>
2024-06-05 19:45:43 +0100

    first commit

---
+++ src/main/java/com/example/songrater/SongraterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.songrater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongraterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongraterBackendApplication.class, args);
+	}
+
+}
